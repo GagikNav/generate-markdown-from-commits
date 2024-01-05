459b94d55cfe8946ca8668ee6b13ed4c9cdadbad
Gagik <gagik.n@gmail.com>
2024-01-05 18:34:28 +0100

    refactor: Refactor import statements and remove unnecessary code

--- index.ts
+++ index.ts
@@ -1,28 +1,27 @@
 import simpleGit from 'simple-git'
-import fs from 'fs'
-import * as core from '@actions/core'
 
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 import process from 'process'
 import * as exec from '@actions/exec'
 import { generateChangelog } from './generateChangelog'
 
-const repoPath = './' // Replace with the path to your Git repository
+
 
 // Get the current branch from the GITHUB_REF environment variable
-// let currentBranch = process.env.GITHUB_REF
-let currentBranch = 'test'
+let currentBranch = process.env.GITHUB_REF
+// let currentBranch = 'test'
 
 // Get the default branch from the github context
-// let defaultBranch: string = github.context.payload.repository?.default_branch
-let defaultBranch: string = 'main'
+let defaultBranch: string = github.context.payload.repository?.default_branch
+// let defaultBranch: string = 'main'
 console.log('Default branch is: ' + defaultBranch)
 
 let changelog: string = ''
 
 // Check if the current branch exists
 if (currentBranch) {
-	// currentBranch = currentBranch.replace('refs/heads/', '')
-	currentBranch = 'test'
+	currentBranch = currentBranch.replace('refs/heads/', '')
 	console.log('Current branch is: ' + currentBranch)
 } else {
 	core.setFailed('Could not determine the current branch')
@@ -32,21 +31,14 @@ if (!defaultBranch) {
 	core.setFailed('Could not determine the default branch')
 }
 
-// Check if the repository exists
-if (!fs.existsSync(repoPath)) {
-	core.setFailed('Repository does not exist')
-}
+
 
 // Get the default branch from the github context
 if (!defaultBranch) {
 	core.setFailed('Could not determine the default branch')
 }
 
-// Check if the repository exists
-if (!fs.existsSync(repoPath)) {
-	console.error('The specified repository does not exist.')
-	process.exit(1)
-}
+
 async function run() {
 	const git = simpleGit();
 	const log = await git.log({
