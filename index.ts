a1b0dc7ae9722fee69ac95e97735d96d1bc10ea6
Gagik <gagik.n@gmail.com>
2024-01-05 18:32:26 +0100

    refactor: Update currentBranch and defaultBranch variables

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import simpleGit from 'simple-git'
 import fs from 'fs'
 import * as core from '@actions/core'
-import * as github from '@actions/github'
+
 import process from 'process'
 import * as exec from '@actions/exec'
 import { generateChangelog } from './generateChangelog'
@@ -9,18 +9,21 @@ import { generateChangelog } from './generateChangelog'
 const repoPath = './' // Replace with the path to your Git repository
 
 // Get the current branch from the GITHUB_REF environment variable
-let currentBranch = process.env.GITHUB_REF
+// let currentBranch = process.env.GITHUB_REF
+let currentBranch = 'test'
 
 // Get the default branch from the github context
-let defaultBranch: string = github.context.payload.repository?.default_branch
+// let defaultBranch: string = github.context.payload.repository?.default_branch
+let defaultBranch: string = 'main'
 console.log('Default branch is: ' + defaultBranch)
 
 let changelog: string = ''
 
 // Check if the current branch exists
 if (currentBranch) {
+	// currentBranch = currentBranch.replace('refs/heads/', '')
+	currentBranch = 'test'
 	console.log('Current branch is: ' + currentBranch)
-	currentBranch = currentBranch.replace('refs/heads/', '')
 } else {
 	core.setFailed('Could not determine the current branch')
 }
@@ -45,36 +48,24 @@ if (!fs.existsSync(repoPath)) {
 	process.exit(1)
 }
 async function run() {
-	let output = ''
-	let error = ''
-	const options = {
-		listeners: {
-			stdout: (data: Buffer) => {
-				output += data.toString()
-			},
-			stderr: (data: Buffer) => {
-				error += data.toString()
-			},
-		},
-	}
+	const git = simpleGit();
+	const log = await git.log({
+			from: defaultBranch,
+			to: currentBranch,
+			format: {
+					hash: '%H',
+					date: '%aI',
+					message: '%s',
+					refNames: '%D',
+					body: '%b',
+					authorName: '%aN',
+					authorEmail: '%aE'
+			}
+	});
 
-	await exec.exec(
-		'git',
-		['log', `--pretty=format:%H %aI %s %D %b %aN %aE`, `${defaultBranch}...${currentBranch}`],
-		options
-	)
-
-	if (error) {
-		console.error(error)
-		return
-	}
-
-	const log = output.split('\n').map((line) => {
-		const [hash, date, message, refNames, body, authorName, authorEmail] = line.split(' ')
-		return { hash, date, message, refNames, body, authorName, authorEmail }
-	})
 	// Process commits and generate Markdown
-	changelog = generateChangelog(log)
+	changelog = generateChangelog(log.all);
+	console.log(changelog)
 }
 run()
 // Set the CHANGELOG environment variable
