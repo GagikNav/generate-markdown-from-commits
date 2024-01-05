bd7a623c48e4975e3c6da9dd445baa435eb6d4e7
Gagik <gagik.n@gmail.com>
2024-01-05 17:20:49 +0100

    chore: Add logging for default and current branch

--- index.ts
+++ index.ts
@@ -12,11 +12,13 @@ let currentBranch = process.env.GITHUB_REF
 
 // Get the default branch from the github context
 let defaultBranch: string = github.context.payload.repository?.default_branch
+console.log('Default branch is: ' + defaultBranch)
 
 let changelog: string = ''
 
 // Check if the current branch exists
 if (currentBranch) {
+	console.log('Current branch is: ' + currentBranch)
 	currentBranch = currentBranch.replace('refs/heads/', '')
 } else {
 	core.setFailed('Could not determine the current branch')
